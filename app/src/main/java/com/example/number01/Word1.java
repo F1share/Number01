853c2b003858e642833473ff035e926112f4605f
F1share <ryota.ishiyama.jobs@gmail.com>
2021-05-18 23:00:15 +0900

    second commit

---
+++ app/src/main/java/com/example/number01/Word1.java
@@ -0,0 +1,14 @@
+package com.example.number01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Word1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_word1);
+    }
+}
